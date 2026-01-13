7e89945ffe825274389c1c782512f62d4039d542
mornedebruyn <mornedebruyn97@gmail.com>
2026-01-13 10:19:03 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0ea5e9",
+        secondary: "#0f172a",
+      },
+    },
+  },
+};
